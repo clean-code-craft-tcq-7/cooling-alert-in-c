baefd3a2da1da80a0473d941e0cc7f416bd74a62
Sudeep <sudeep.rp@gmail.com>
2025-09-06 13:52:47 +0530

    step 1 and 2

---
+++ cooling_alert.c
@@ -0,0 +1,24 @@
+#include "./cooling_alert.h"
+#include <stdio.h>
+
+// ----------- STEP 2 -----------------
+// Minimal code to compile and fail the test in STEP 2 of test_cooling_alert.cpp
+
+// const float PASSIVE_COOLING_LIMIT = 40.0;
+
+// Action battery_data_to_action(CoolingType cooling_type, float temperature) {
+//     Action action;
+//     // ------- STEP 2.1 -----------------
+//     // Minimal code to pass STEP 2 in test_cooling_alert.cpp
+//     // if (cooling_type == PASSIVE_COOLING && temperature > PASSIVE_COOLING_LIMIT) {
+//     //     action.actionType = ALERT_EMAIL;
+//     //     snprintf(action.actionBody, sizeof(action.actionBody),
+//     //         "Temperature alert: %.1fF", temperature);
+//     // } else {
+//     //     action.actionType = NO_ALERT;
+//     //     action.actionBody[0] = '\0';
+//     // }
+//     //------- STEP 2.1 code ends -------
+//     return action;
+// }
+// ----------- STEP 2 code ends -------
---
+++ test_cooling_alert.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+extern "C" {
+#include "./cooling_alert.h"
+}
+
+// ----------- STEP 2 -----------------
+// Establish one "straight forward" interface (happy flow):
+//   - Function name
+//   - Input parameters data-model
+//   - Expected output data-model
+
+// TEST(CoolingAlertTest, PassiveCoolingAlertToEmail) {
+//     Action action = battery_data_to_action(PASSIVE_COOLING, 45.0f);
+//     ASSERT_EQ(action.actionType, ALERT_EMAIL);
+//     ASSERT_STREQ(action.actionBody, "Temperature alert: 45.0F");
+// }
+
+// TEST(CoolingAlertTest, NoAlert) {
+//     Action action = battery_data_to_action(PASSIVE_COOLING, 32.0f);
+//     ASSERT_EQ(action.actionType, NO_ALERT);
+// }
+// ----------- STEP 2 code ends -------
