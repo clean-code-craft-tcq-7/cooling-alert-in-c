cfc84c30a9f1bff9ce4dfa65f1a7214d95425dc8
Manoj Mohan <haimanojkumar@gmail.com>
2025-09-21 07:25:26 +0530

    Update battery id

--- test_classify.cpp
+++ test_classify.cpp
@@ -7,13 +7,13 @@ extern "C" {
 // Separation of classification from the email action.
 
 // TEST(ClassifyTest, BatteryOk) {
-//     BatteryDataModel batteryData = {1, THERMAL_PASSIVE, 25};
+//     BatteryDataModel batteryData = {10567, THERMAL_PASSIVE, 25};
 //     BatteryState state = classify_battery_state(batteryData);
 //     ASSERT_EQ(state, BATTERY_OK);
 // }
 
 // TEST(ClassifyTest, BatteryAlert) {
-//     BatteryDataModel batteryData = {2, THERMAL_HYBRID, 60};
+//     BatteryDataModel batteryData = {21389, THERMAL_HYBRID, 60};
 //     BatteryState state = classify_battery_state(batteryData);
 //     ASSERT_EQ(state, BATTERY_ALERT);
 // }
