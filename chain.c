1c07265cc8e7147d0ecff251edd82841d31812fa
Manoj Mohan <haimanojkumar@gmail.com>
2025-09-23 08:50:37 +0530

    invoke classifier

--- chain.c
+++ chain.c
@@ -1,12 +1,14 @@
 #include "./chain.h"
+#include "./classify.h"
 #include "./thresholds.h"
 
 // ----------- STEP 6 code starts -------
 
-// void batteryDataToAction(BatteryDataModel batteryData, Actuators (*actuators)()) {
-//     if (batteryData.temperature > alertTemperatureForCoolingType(batteryData.thermalManagementType)) {
-//         actuators().emailSender("manager@battery.com", "Battery Alert", "Battery temperature is too high", "noreply@battery.com");
-//     }
-// }
+//void batteryDataToAction(BatteryDataModel batteryData, Actuators (*actuators)()) {
+//   BatteryState state = classify_battery_state(batteryData);
+//   if (state == BATTERY_ALERT) {
+//        actuators().emailSender("manager@battery.com", "Battery Alert", "Battery temperature is too high", "noreply@battery.com");
+//   }
+//}
 
 // ----------- STEP 6 code ends -------
